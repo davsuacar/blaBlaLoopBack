b934fca094aa55702b341c4b4cbf9d3d0888d8fb
David Suarez <david.suarez@intelygenz.com>
2015-07-27 16:25:56 +0200

    Remote method

--- common/models/car.js
+++ common/models/car.js
@@ -8,7 +8,8 @@ module.exports = function(Car) {
   Car.remoteMethod(
     'myLicensePlate',
     {
-      accept: {arg: 'numberPlate', type: 'string'},
+      description: 'License Plate Service',
+      accepts: {arg: 'numberPlate', type: 'number', http: {source: 'query'}, description: 'License Plate'},
       returns: {arg: 'numberPlate', type: 'string'},
       http: {path: '/license', verb: 'get'}
     }
